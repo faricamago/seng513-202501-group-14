211e7159e9d19ea969f70832603d5916e888129c
briggscole <cole.a.briggs@gmail.com>
2025-03-18 18:22:34 -0600

    Made navbar slightly larger

--- frontend/dinoverse/src/components/navbar.tsx
+++ frontend/dinoverse/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className={`fixed top-0 left-0 w-full h-16 z-40 bg-[var(--primary-pink)] flex items-center`}>
+    <nav className={`fixed top-0 left-0 w-full h-20 z-40 bg-[var(--primary-pink)] flex items-center`}>
       <div className="w-1/8 items-center justify-items-center">
         <img src="/assets/DinoLogo.png" alt="Logo" className="w-20 h-[calc(100%-16px)]" />
       </div>
