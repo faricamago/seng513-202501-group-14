cb5f221b07611ab55d6479000c3d1a9e6b930633
briggscole <cole.a.briggs@gmail.com>
2025-03-20 13:31:08 -0600

    Renamed "Navbar" to "Header" in page layout

--- frontend/src/app/(pages)/layout.tsx
+++ frontend/src/app/(pages)/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/footer';
-import Navbar from '@/components/navbar';
+import Header from '@/components/header';
 
 const PageLayout = ({
     children
@@ -9,7 +9,7 @@ const PageLayout = ({
 
     return (
         <div className={`h-screen w-screen flex flex-col overflow-y-clip`}>
-            <Navbar/>
+            <Header/>
             <main className={`absolute top-20 bottom-16 w-full z-0 bg-white overflow-y-scroll py-4 px-32`}>
                 {children}
             </main>
