f7f53faee8ee693edcb173a6bc612900e8be3585
aarushirc14 <AarushiRC14@gmail.com>
2025-03-23 19:38:48 -0600

    making text black

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
           <img src="/assets/DinoLogo.png" alt="Logo" className="w-20 h-[calc(100%-16px)]" />
         </Link>
         <div className="w-6/8 flex items-center justify-center">
-          <input type="text" placeholder="Search..." className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--verylight-pink)] bg-white/50"/>
+          <input type="text" placeholder="Search..." className="w-full px-4 py-2 border border-gray-300 text-[var(--dark-color)] rounded-md focus:outline-none focus:ring-2 focus:ring-[var(--verylight-pink)] bg-white/50"/>
         </div>
         {username && (<button onClick={() => setShowLogoutModal(true)} className="w-1/8 flex items-center justify-center"><LuLogOut className="text-3xl text-white" /></button>
         )}
@@ -39,10 +39,10 @@ const Header = () => {
       {showLogoutModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-50">
           <div className="bg-white p-8 rounded-xl shadow-2xl max-w-md mx-auto transform transition-all duration-300 hover:scale-105">
-            <h2 className="text-2xl font-bold text-center mb-4">Confirm Logout</h2>
+            <h2 className="text-[var(--dark-color)] text-2xl font-bold text-center mb-4">Confirm Logout</h2>
             <p className="text-gray-700 text-center mb-6">Are you sure you want to log out?</p>
             <div className="flex justify-center space-x-4">
-              <button onClick={() => setShowLogoutModal(false)} className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400 transition">Cancel</button>
+              <button onClick={() => setShowLogoutModal(false)} className="px-4 py-2 bg-gray-300 text-[var(--dark-color)] rounded hover:bg-gray-400 transition">Cancel</button>
               <button onClick={confirmLogout} className="px-4 py-2 bg-[var(--primary-pink)] text-white rounded hover:bg-[var(--bright-pink)] transition">
                 Confirm
               </button>
