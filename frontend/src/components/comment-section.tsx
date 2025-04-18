abedabef3c20f3d31b887a61ceab9b783b990d39
Tusher <khsaminrashid@gmail.com>
2025-04-17 19:22:58 -0600

    empty comment section just to pull origin

--- frontend/src/components/comment-section.tsx
+++ frontend/src/components/comment-section.tsx
@@ -2,4 +2,8 @@ import React, { useState, useEffect } from "react";
 
 interface CommentSectionProps {}
 
-const CommentSection: React.FC<CommentSectionProps> = () => {};
+const CommentSection: React.FC<CommentSectionProps> = () => {
+  <div className="mt-4 border-t pt-4">
+    <h4 className="font-bold mb-2">Comments</h4>P
+  </div>;
+};
