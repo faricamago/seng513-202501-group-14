c44809fccc089c443726fa99311917054c7a0f3b
faricamago <farica.mago@ucalgary.ca>
2025-04-19 21:09:04 -0600

    icon change for login

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { LuLogOut } from "react-icons/lu";
 import { MdAdminPanelSettings } from "react-icons/md";
 import { useRouter } from 'next/navigation';
+import { MdLogin } from "react-icons/md";
 
 const Header = () => {
   const [username, setUsername] = useState('');
@@ -99,9 +100,7 @@ const Header = () => {
             </button>
           ) : (
             <Link href="/login" className="p-2 rounded-md hover:bg-white/20 transition relative group flex items-center justify-center">
-              <span className="text-white text-base font-bold group-hover:text-[var(--uoc-yellow)]">
-                Login
-              </span>
+              <MdLogin className="text-2xl text-white group-hover:text-[var(--uoc-yellow)]" />
               <span className="absolute left-1/2 -translate-x-1/2 top-full mt-1 px-2 py-1 text-xs text-white bg-black rounded opacity-0 group-hover:opacity-100 transition-opacity">
                 Login
               </span>
